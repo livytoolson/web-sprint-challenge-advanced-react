7ad24a9836e7114dbd06a35d6796d53a062b81d6
Olivia Toolson <livytoolson@gmail.com>
2020-10-30 09:35:46 -0700

    two tests passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,53 +4,44 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-// test("compoonent renders correctly", () => {
-//     render(<CheckoutForm />);
-// })
+test("compoonent renders correctly", () => {
+    render(<CheckoutForm />);
+})
 
 test("form header renders", () => {
-    // render(<CheckoutForm />);
+    render(<CheckoutForm />);
 
-    // screen.getByLabelText(/checkout form/i);
+    screen.getByText(/checkout form/i);
 });
-
-// test("'plants' and 'cart' buttons render", () => {
-//     render(<CheckoutForm />);
-
-//     const plantsButton = screen.getByRole('button', { name: /plants/i });
-//     const cartButton = screen.getByRole('button', { name: /cart/i });
-
-//     expect(plantsButton).toBeInTheDocument();
-//     expect(cartButton).toBeInTheDocument();
-// })
 
 test("form shows success message on submit with form details", () => {
-    // render(<CheckoutForm />);
+    render(<CheckoutForm />);
 
-    // const firstNameInput = screen.getByLabelText(/first name/i);
-    // const lastNameInput = screen.getByLabelText(/last name/i);
-    // const addressInput = screen.getByLabelText(/address/i);
-    // const cityInput = screen.getByLabelText(/city/i);
-    // const stateInput = screen.getByLabelText(/state/i);
-    // const zipInput = screen.getByLabelText(/zip/i);
+    const firstNameInput = screen.getByText(/first name/i);
+    const lastNameInput = screen.getByText(/last name/i);
+    const addressInput = screen.getByText(/address/i);
+    const cityInput = screen.getByText(/city/i);
+    const stateInput = screen.getByText(/state/i);
+    const zipInput = screen.getByText(/zip/i);
 
-    // fireEvent.change(firstNameInput, { target: { value: 'John'} });
-    // fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
-    // fireEvent.change(addressInput, { target: { value: '1234 Abc Way' } });
-    // fireEvent.change(cityInput, { target: { value: 'Los Angeles' } });
-    // fireEvent.change(stateInput, { target: { value: 'CA' } });
-    // fireEvent.change(zipInput, { target: { value: '12345' } });
+    fireEvent.change(firstNameInput, { target: { value: 'John'} });
+    fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
+    fireEvent.change(addressInput, { target: { value: '1234 Abc Way' } });
+    fireEvent.change(cityInput, { target: { value: 'Los Angeles' } });
+    fireEvent.change(stateInput, { target: { value: 'CA' } });
+    fireEvent.change(zipInput, { target: { value: '12345' } });
 
-    // expect(firstNameInput).toHaveValue('John');
-    // expect(lastNameInput).toHaveValue('Doe');
-    // expect(addressInput).toHaveValue('1234 Abc Way');
-    // expect(cityInput).toHaveValue('Los Angeles');
-    // expect(stateInput).toHaveValue('CA');
-    // expect(zipInput).toHaveValue('12345');
+    expect(firstNameInput).toHaveValue('John');
+    expect(lastNameInput).toHaveValue('Doe');
+    expect(addressInput).toHaveValue('1234 Abc Way');
+    expect(cityInput).toHaveValue('Los Angeles');
+    expect(stateInput).toHaveValue('CA');
+    expect(zipInput).toHaveValue('12345');
 
-    // const checkoutButton = screen.getByRole('button', { name: /checkout/i });
-    // fireEvent.click(checkoutButton);
+    const checkoutButton = screen.getByRole('button', { name: /checkout/i });
+    fireEvent.click(checkoutButton);
 
-    // const successMessage = screen.getByText(/you have ordered some new plants! woo-hoo!/i);
-    // expect(successMessage).toBeInTheDocument();
+    const successMessage = screen.getByText(/you have ordered some new plants! woo-hoo!/i);
+    expect(successMessage).toBeInTheDocument();
 });
+
